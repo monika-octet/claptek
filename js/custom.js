6d27adcb5aa080a1d5128af0be6fbe0db0f958bd
Abbhishek Soni <abbhisheksoni001@gmail.com>
2022-12-14 17:56:11 +0530

    Ui feedback points

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,16 @@ var $selectValue = $('#select_value').find('strong');
 $selectValue.text($('#get_value').val());
 
 
-$('#get_value').selectric().on('change', function() {
-  $selectValue.text($(this).val());
-});
+// $('#get_value').selectric().on('change', function() {
+//   $selectValue.text($(this).val());
+// });
+
+$('.removeToDashboard').on('click', function(){
+  $('.selected-user').fadeOut()
+  window.location.href = "https://claptek.vercel.app/business-user/pendancy-dashboard.html"
+})
+
+$(".workflow-block").hide();
+$('#workflow').on('click', function(){
+  $(".workflow-block").show();
+})
