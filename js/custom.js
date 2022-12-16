df7865701e0380388b0a29575549b4e6b59a3711
Abbhishek Soni <abbhisheksoni001@gmail.com>
2022-12-16 16:21:12 +0530

    QC points

--- js/custom.js
+++ js/custom.js
@@ -23,3 +23,8 @@ $('.add-new-rules-btn').on('click', function(){
   $('#allocated-rules-tab-pane').toggleClass('show active')
   $('#add-rules-tab-pane').toggleClass('show active')
 })
+
+$('.rule-list-section').hide();
+$('.btn-add-rule-save').on('click', function(){
+  $('.rule-list-section').fadeIn();
+})
